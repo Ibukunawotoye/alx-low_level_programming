fe98f3733025a83f98a3ade6d70c5876cd21ab32
Ibukunawotoye <awotoyeibukunoluwa@gmail.com>
2023-04-24 09:48:03 -0700

    Combs

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int la, dp;
 
-	for (la = 0; la < 9; la++)
+	for (la = '0'; la < '9'; la++)
 	{
-	for (dp = la + 1; dp <= 9; dp++)
+	for (dp = la + '1'; dp <= '9'; dp++)
 	{
 	if (dp != la)
 	{
 	putchar(la);
 	putchar(dp);
-	if (la == 8 && dp == 9)
+	if (la == '8' && dp == '9')
 	continue;
 	putchar(',');
 	putchar(' ');
