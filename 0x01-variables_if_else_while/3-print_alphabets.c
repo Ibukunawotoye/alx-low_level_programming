2873d08c79356dc08219f3cddae3e02eeea8fd77
Ibukunawotoye <awotoyeibukunoluwa@gmail.com>
2023-04-23 18:54:39 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase and uppercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ib;
+
+	for (ib = 'a'; ib <= 'z'; ib++)
+		putchar(ib)
+
+	for (ib = 'A'; ib <= 'Z'; ib++)
+		putchar(ib)
+
+	putchar('\n');
+
+	return (0);
+}
