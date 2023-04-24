bec64aa02dae54682f7232bef2afe7a473e4d629
Ibukunawotoye <awotoyeibukunoluwa@gmail.com>
2023-04-23 18:37:43 -0700

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.>
+
+/**
+ * main - print alphabet in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ib;
+
+	for (ib = 'a'; ib <= 'z'; ib++)
+	{
+		putchar(ib);
+	}
+	{
+		putchar('\n');
+		return (0);
+}
