2b79d5d87b1f36dc351eb5d0ce5eb30cff43ca0c
Ibukunawotoye <awotoyeibukunoluwa@gmail.com>
2023-04-24 09:43:57 -0700

    Combs

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int la, dp;
+
+	for (la = 0; la < 9; la++)
+	{
+	for (dp = la + 1; dp <= 9; dp++)
+	{
+	if (dp != la)
+	{
+	putchar(la);
+	puthcar(dp);
+	if (la == 8 && dp == 9)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
