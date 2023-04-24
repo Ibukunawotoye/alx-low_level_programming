52fa4e138094b3b22135116e095a60eeed16a982
Ibukunawotoye <awotoyeibukunoluwa@gmail.com>
2023-04-24 10:59:28 -0700

    Brains

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,13 +6,15 @@
  * main - prints all possible different combinations of two digits
  * Return: Always 0
  */
+
 int main(void)
+
 {
 	int la, dp;
 
 	for (la = '0'; la < '9'; la++)
 	{
-	for (dp = la + '1'; dp <= '9'; dp++)
+	for (dp = la + 1; dp <= '9'; dp++)
 	{
 	if (dp != la)
 	{
