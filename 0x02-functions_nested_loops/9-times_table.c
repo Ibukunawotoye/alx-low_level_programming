ad72a7d59573f3e86f0d3d6e2a5934c2a808b479
Ibukunawotoye <awotoyeibukunoluwa@gmail.com>
2023-04-26 20:27:21 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table
+ */
+void times_table(void)
+{
+	int ib, ik, ip;
+
+	for (ib = 0; ib <= 9; ib++)
+	{
+		_putchar('0');
+
+		for (ik = 1; ik <= 9; ik++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			ip = ib * ik;
+
+			if (ip <= 9)
+			   _putchar(' ');
+			else
+				_putchar((ip / 10) + '0');
+			_putchar((ip % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
