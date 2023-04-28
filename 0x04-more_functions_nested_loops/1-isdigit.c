041be520333639b3f9e9c3823e29055031987803
Ibukunawotoye <awotoyeibukunoluwa@gmail.com>
2023-04-27 20:31:55 -0700

    I supper

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: character to be checked
+ * Return: 1 if c is a digit returns 0 otherwise
+ */
+int _isdigit(int c)
+{
+
