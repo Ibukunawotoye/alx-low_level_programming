a06eb3ec05634f002cd3fadb073fa113aee867ed
Ibukunawotoye <awotoyeibukunoluwa@gmail.com>
2023-06-17 19:39:08 -0700

    trying

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t ib;
+	ssize_t w;
+	ssize_t i;
+
+	fd = open(filename, O_RDONLY);
+	if (ib == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	i = read(ib, buf, letters);
+	w = write(STDOUT_FILENO, buf, i);
+
+	free(buf);
+	close(ib);
+	return (w);
+}
